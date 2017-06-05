b2e3a51a803f3186bbccc110b5722d8140095c77
e-arslan <earslan@tamu.edu>
2017-06-04 23:09:56 -0500

    Add files via upload

---
+++ time.R
@@ -0,0 +1,41 @@
+
+# Get the current date: today
+today = Sys.Date()
+
+# See what today looks like under the hood
+unclass(today)
+
+# Get the current time: now
+now = Sys.time()
+
+# See what now looks like under the hood
+unclass(now)
+
+
+# Definition of character strings representing dates
+str1 <- "May 23, '96"
+str2 <- "2012-03-15"
+str3 <- "30/January/2006"
+
+# Convert the strings to dates: date1, date2, date3
+date1 <- as.Date(str1, format = "%b %d, '%y")
+date2 <- as.Date(str2)
+date3 <- as.Date(str3, format = "%d/%B/%Y")
+
+# Convert dates to formatted strings
+format(date1, "%A")
+format(date2,"%d")
+format(date3,"%b %Y")
+
+
+# Definition of character strings representing times
+str1 <- "May 23, '96 hours:23 minutes:01 seconds:45"
+str2 <- "2012-3-12 14:23:08"
+
+# Convert the strings to POSIXct objects: time1, time2
+time1 <- as.POSIXct(str1, format = "%B %d, '%y hours:%H minutes:%M seconds:%S")
+time2 <- as.POSIXct(str2)
+
+# Convert times to formatted strings
+format(time1,"%M")
+format(time2,"%I %M %p")
